7924a78bf69ff3e304a518dd9d1705e6ea90de3f
abraao-reyys <146043864+abraao-reyys@users.noreply.github.com>
2024-12-24 10:21:02 -0300

    aula11 upd

--- aula11/ex010.js
+++ aula11/ex010.js
@@ -9,12 +9,17 @@
 function calcular() {
     var vel = Number(document.getElementById('ivel').value);
     var ires = document.getElementById('ires');
+
+    ires.style.padding = '5px'
+    ires.style.border = '1px solid black'
     
     if (vel <= 60) {
         ires.innerText = 'Essa velocidade não gerou multas.'
+        ires.style.color = 'green'
     } else {
         var result = (vel - 60) * 6.2
         ires.innerText = `Essa velocidade gerou uma multa de R$ ${result.toFixed(2)}.`
+        ires.style.color = 'red'
     }
 }
 
@@ -23,4 +28,6 @@ function limpar() {
     var ires = document.querySelector('div#ires');
     vel.value = ''
     ires.innerHTML = ''
+    ires.style.padding = '0'
+    ires.style.border = '0'
 }
