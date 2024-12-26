c34b0af90a609569d6baa13a53a5ca444fa76dbb
abraao-reyys <146043864+abraao-reyys@users.noreply.github.com>
2024-12-26 01:40:41 -0300

    aula12ex upd

--- aula12ex/modelo2/script.js
+++ aula12ex/modelo2/script.js
@@ -5,19 +5,19 @@ var imgbox = window.document.getElementById('img')
 var body = window.document.getElementsByTagName('body')[0]
 
 if (hour <= 4 && hour >= 0) {
-    textbox.innerHTML = `Agora são <strong>${hour} horas</strong> da madrugada.`
+    textbox.innerHTML = `Agora é <strong>${hour} hora (s)</strong> da madrugada.`
     imgbox.style.backgroundImage = 'url(images/dawn-mod.jpg)'
     body.style.backgroundColor = '#080705'
 } else if (hour < 12) {
-    textbox.innerHTML = `Agora são <strong>${hour} horas</strong> da manhã.`
+    textbox.innerHTML = `Agora é <strong>${hour} hora (s)</strong> da manhã.`
     imgbox.style.backgroundImage = 'url(images/day-mod.jpg)'
     body.style.backgroundColor = '#EEA47A'
 } else if (hour < 18) {
-    textbox.innerHTML = `Agora são <strong>${hour} horas</strong> da tarde.`
+    textbox.innerHTML = `Agora é <strong>${hour} hora (s)</strong> da tarde.`
     imgbox.style.backgroundImage = 'url(images/afternoon-mod.jpg)'
     body.style.backgroundColor = '#C6B29E'
 } else {
-    textbox.innerHTML = `Agora são <strong>${hour} horas</strong> da noite.`
+    textbox.innerHTML = `Agora é <strong>${hour} hora (s)</strong> da noite.`
     imgbox.style.backgroundImage = 'url(images/night-mod.jpg)'
     body.style.backgroundColor = '#273C4F'
 }
