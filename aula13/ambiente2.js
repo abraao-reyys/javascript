458934d945a5b1c2dacf9ae6a1842c4f5b544a55
abraao-reyys <146043864+abraao-reyys@users.noreply.github.com>
2024-12-27 19:21:18 -0300

    aula13 upd

---
+++ aula13/ambiente2.js
@@ -0,0 +1,8 @@
+// Terceiro teste de Repetições
+
+var total_messages = 10 // quantidade de mensagens que eu desejo enviar
+
+do {
+    total_messages -- // diminui 1 a cada repetição
+    console.log('Tudo bem?')
+} while (total_messages != 0) // estrutura de repetição invertida, onde primeiro executa e depois testa
