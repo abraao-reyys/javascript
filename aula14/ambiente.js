540758ed5e30d44925a7f4164c78c51d2fdf4b02
abraao-reyys <146043864+abraao-reyys@users.noreply.github.com>
2024-12-27 23:38:48 -0300

    aula14 upd

---
+++ aula14/ambiente.js
@@ -0,0 +1,27 @@
+/* Usando 'while' para exemplificação -----
+
+var count = 0
+
+while (count < 4) {
+    console.log(count)
+    count ++
+} */
+
+
+
+/* Usando 'do' e 'while' ------------------
+
+var count = 0
+
+do {
+    console.log(count)
+    count++
+} while (count < 4) */
+
+
+
+// Usando o 'for' -------------------------
+
+for (var count = 1 ; count <= 5 ; count++) {
+    console.log(count)
+}
