b0db2e529b78fec9096405c36b85f3d73d1dfd89
abraao-reyys <146043864+abraao-reyys@users.noreply.github.com>
2024-12-29 14:05:14 -0300

    aula14ex upd

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,4 +1,4 @@
-var generate_button = window.document.querySelector('div.input-box > input:nth-of-type(2)')
+var generate_button = window.document.querySelector('div#output > input:nth-of-type(1)')
 var output_box = window.document.querySelector('div#output > p')
 
 generate_button.addEventListener('click', toGenerate)
@@ -6,5 +6,13 @@ generate_button.addEventListener('click', toGenerate)
 function toGenerate() {
     var num = Number(window.document.getElementById('inum').value)
 
-    
+    output_box.innerHTML = ''
+
+    if (num == "") {
+        output_box.innerHTML = 'Não foi adicionado um número. Tente novamente.'
+    } else {
+        for (num; num <= 10; num++) {
+            // Parei aqui.
+        }
+    }
 }
