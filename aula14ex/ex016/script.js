5297e8b6847796888e81c82f40c26a58d4f35250
abraao-reyys <146043864+abraao-reyys@users.noreply.github.com>
2024-12-29 12:42:47 -0300

    aula14ex upd

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,8 +1,10 @@
 
 var count_button = window.document.querySelector('div.input-button > input:nth-of-type(1)')
+var reset_button = window.document.querySelector('div.input-button > input:nth-of-type(2)')
 var output_box = window.document.querySelector('div#output > p')
 
 count_button.addEventListener('click', toCount)
+reset_button.addEventListener('click', toClear)
 
 function toCount() {
     var start = Number(window.document.getElementById('istart').value)
@@ -16,4 +18,15 @@ function toCount() {
     }
 
     output_box.innerHTML += '&#x1F3C1; Fim!'
+}
+
+function toClear() {
+    var start = window.document.getElementById('istart')
+    var end = window.document.getElementById('iend')
+    var step = window.document.getElementById('istep')
+
+    start.value = ''
+    end.value = ''
+    step.value = ''
+    output_box.innerHTML = 'Preparando a contagem...'
 }
