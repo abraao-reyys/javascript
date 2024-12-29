8231f65a2e3d58689dbd78ff5747c0404d50b51d
abraao-reyys <146043864+abraao-reyys@users.noreply.github.com>
2024-12-29 15:17:10 -0300

    aula14ex upd

---
+++ aula14ex/ex017/logictest.js
@@ -0,0 +1,14 @@
+/*
+if (num == "") {
+    output_box.innerHTML = 'Não foi adicionado um número. Tente novamente.'
+} else {
+    for (c = 1; c <= 10; c++) {
+        output_box.innerHTML = `${num} * ${c} = ${num * c} <br>`
+    }
+} */
+
+var num = 5
+
+for (c = 1; c <= 10; c++) {
+    console.log(`${num} * ${c} = ${num * c}`)
+}
--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,7 +1,9 @@
 var generate_button = window.document.querySelector('div#output > input:nth-of-type(1)')
+var reset_button = window.document.querySelector('div#output > input:nth-of-type(2)')
 var output_box = window.document.querySelector('div#output > p')
 
 generate_button.addEventListener('click', toGenerate)
+reset_button.addEventListener('click', toClear)
 
 function toGenerate() {
     var num = Number(window.document.getElementById('inum').value)
@@ -11,8 +13,15 @@ function toGenerate() {
     if (num == "") {
         output_box.innerHTML = 'Não foi adicionado um número. Tente novamente.'
     } else {
-        for (num; num <= 10; num++) {
-            // Parei aqui.
+        for (c = 1; c <= 10; c++) {
+            output_box.innerHTML += `${num} * ${c} = ${num * c} <br>`
         }
     }
+}
+
+function toClear() {
+    var num = window.document.getElementById('inum')
+
+    num.value = ''
+    output_box.innerHTML = 'Digite um número para conferir sua tabuada...'
 }
