96554b31b487e075ebec7cf88ba50a2d9f109dbd
abraao-reyys <146043864+abraao-reyys@users.noreply.github.com>
2024-12-29 13:55:56 -0300

    aula14ex upd

---
+++ aula14ex/ex017/script.js
@@ -0,0 +1,10 @@
+var generate_button = window.document.querySelector('div.input-box > input:nth-of-type(2)')
+var output_box = window.document.querySelector('div#output > p')
+
+generate_button.addEventListener('click', toGenerate)
+
+function toGenerate() {
+    var num = Number(window.document.getElementById('inum').value)
+
+    
+}
