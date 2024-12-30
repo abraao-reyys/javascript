ba3fdd37e71c729e9831a542d6e26d1db0fc0cc1
abraao-reyys <146043864+abraao-reyys@users.noreply.github.com>
2024-12-30 18:54:21 -0300

    aula16 upd

---
+++ aula16/ambiente2.js
@@ -0,0 +1,13 @@
+var res = 1
+
+function fatorial(n) {
+    for (let c = n; c != 1; c--) {
+        // console.log(`${c} * `)
+        process.stdout.write(`${c} * `) // Para deixar na mesma linha do terminal
+        res *= c
+    }
+    process.stdout.write("1 = ") // Para adicionar no final da linha
+    return res
+}
+
+console.log(fatorial(5))
