f77e7351b251234ec600aa5965df3fc01e6a3017
abraao-reyys <146043864+abraao-reyys@users.noreply.github.com>
2025-01-03 17:36:02 -0300

    aula16ex upd

---
+++ aula16ex/ex1/logictest.js
@@ -0,0 +1,3 @@
+let numlist = [3, 3, 4, 5]
+
+console.log(Math.min(...numlist))
--- aula16ex/ex1/script.js
+++ aula16ex/ex1/script.js
@@ -1,19 +1,40 @@
 var add_button = window.document.querySelector('div.input-box > input[type="submit"]')
 var fin_button = window.document.querySelector('div.send-box > input')
 var output_box = window.document.querySelector('div#output-box')
+var select_list = window.document.getElementById('isetnum')
 
 add_button.addEventListener('click', addNumber)
+fin_button.addEventListener('click', analysisFinal)
+
+var numlist = []
 
 function addNumber() {
     output_box.innerHTML = ''
     let num = Number(window.document.getElementById('inum').value)
-    let select_list = window.document.getElementById('isetnum')
     let op = document.createElement('option')
-    if (num >= 1 && num <= 100) {
+    if (numlist.indexOf(num) != -1) {
+        window.alert('Valor já encontrado na lista.')
+    } else if (num >= 1 && num <= 100) {
+        numlist.push(num)
         op.text = `Valor ${num} adicionado.`
         select_list.appendChild(op)
     } else {
-        output_box.innerHTML = 'Adicione um número entre 1 e 100'
+        window.alert('Valor inválido.')
     }
-    
+}
+
+function analysisFinal() {
+    output_box.innerHTML = ''
+    let sum_result = 0
+    for (let c = 0; c < numlist.length; c++) {
+        sum_result += numlist[c]
+    }
+    let media = sum_result / numlist.length
+    let p = document.createElement('p')
+    if (numlist.length < 1) {
+        window.alert('Adicione algum valor para poder finalizar.')
+    } else {
+        p.innerHTML = `Ao todo, temos ${numlist.length} números cadastrados. <br> O maior valor informado foi ${Math.max(...numlist)}. <br> O menor valor informado foi ${Math.min(...numlist)}. <br> Somando todos os valores, temos ${sum_result}. <br> A média dos valores digitados é ${media}.`
+        output_box.appendChild(p)
+    }
 }
